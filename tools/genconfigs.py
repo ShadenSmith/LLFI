dcb1c05bdcefbeaea61ca4d97b80185126abde12
Shaden Smith <ShadenTSmith@gmail.com>
2014-01-13 13:53:31 -0600

    Now using argparser and added new parameters.

--- tools/genconfigs.py
+++ tools/genconfigs.py
@@ -4,12 +4,25 @@ import os
 import sys
 import errno
 import re
+import argparse
 
-# Commonly used parameters
-params = {
-  "llfi" : None,
-  "verbose" : False,
-}
+# Commonly used parameters - see processArgs()
+params = {}
+
+def processArgs():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('DIR', help='base LLFI output directory')
+  parser.add_argument('-n' , '--nconfigs', help='number of configs to print',
+                      default=1)
+  parser.add_argument('-c' , '--code', help='return code to filter by',
+                      default=-11)
+  parser.add_argument('-g' , '--group', help='run group ID to scan',
+                      default=0)
+  parser.add_argument('-v' , '--verbose', default=False)
+  args = parser.parse_args()
+
+  for param in args.__dict__:
+    params[param] = args.__dict__[param]
 
 def getBasePath():
   """
@@ -28,7 +41,7 @@ def getErrorFiles(group):
     a given group of runs.
   """
   regex = re.compile(".*run-{}-(\d+)$".format(group))
-  path = os.path.join(params["llfi"], "error_output")
+  path = os.path.join(params["DIR"], "error_output")
   files = os.listdir(path)
   files = [os.path.join(path, f) for f in files if regex.match(f)]
   return files
@@ -63,7 +76,7 @@ def getConfig(group=0, runID=0):
 
   keyval = re.compile("^(\w+)=(\w+),?$")
 
-  path = os.path.join(params["llfi"], "llfi_stat_output")
+  path = os.path.join(params["DIR"], "llfi_stat_output")
   name = "llfi.stat.fi.injectedfaults.{}-{}.txt".format(group,runID)
   fname = os.path.join(path, name)
   with open(fname) as f:
@@ -83,25 +96,26 @@ def printConfigs(configs):
   """
     Pretty prints a list of configs to be used for future use by LLFI.
   """
+  i = 0
   for conf in configs:
+    if i == params["nconfigs"]:
+      break
     for (key,val) in conf.items():
       print "{}={}".format(key,val)
     print ""
-
+    i += 1
 
 if __name__ == '__main__':
-  params["llfi"] = getBasePath()
+  processArgs()
 
   # TODO: Get input from ARGV
-  code = -11
   if(params["verbose"]):
-    print "Processing runs with return code: {}".format(code)
+    print "Processing runs with return code: {}".format(params["code"])
 
-  err_runs = getErrorRunIDs(code=code)
+  err_runs = getErrorRunIDs(group=params["group"], code=params["code"])
   if(params["verbose"]):
     print "Run IDs: {}".format(" ".join(err_runs))
 
   configs = [getConfig(runID=r) for r in err_runs]
   printConfigs(configs)
-
 
